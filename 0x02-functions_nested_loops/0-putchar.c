25ffdc4c29d0695a8af57657577220963e3f0ca3
Kayciegh <kadichamussiane@gmail.com>
2023-06-20 15:44:20 -0700

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
+*/
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Solving funtions and nested loops
